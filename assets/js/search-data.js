959698aa366bef4a9ddd9a239aa43a2dcc386b48
vjoshy <80487875+vjoshy@users.noreply.github.com>
2026-08-05 09:20:01 +0000

    Deploying to gh-pages from @ vjoshy/vjoshy.github.io@c51344cb3572d22a2e48968c5854f512db187b09 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,55 +45,13 @@ ninja.data = [{
             },},{id: "news-graduated-from-bmath-hons-mathematical-science",
           title: 'Graduated from BMath.(Hons) Mathematical Science!',
           description: "",
-          section: "News",},{id: "news-started-msc-statistics-at-university-of-guelph",
-          title: 'Started MSc. Statistics at University of Guelph',
+          section: "News",},{id: "news-started-msc-applied-statistics-at-university-of-guelph",
+          title: 'Started MSc. Applied Statistics at University of Guelph',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{
+          section: "News",},{id: "news-completed-msc-applied-statistics-at-university-of-guelph",
+          title: 'Completed MSc. Applied Statistics at University of Guelph',
+          description: "",
+          section: "News",},{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
